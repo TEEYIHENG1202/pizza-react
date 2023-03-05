d0930c6bc1a785f1503bd39f6ad09875d923b3d6
TEEYIHENG1202 <124483764+TEEYIHENG1202@users.noreply.github.com>
2023-03-05 17:42:35 +0800

    Add files via upload

---
+++ src/component/Result.js
@@ -0,0 +1,30 @@
+import userEvent from '@testing-library/user-event';
+import { useContext,useState } from 'react';
+import Calculation from './Calculation';
+
+function Result({orders,show,setShow,UserContext})
+{
+    const user = useContext(UserContext);
+    const [total,setTotal] = useState(0);
+
+return (<>
+    <>
+    {show?
+        <div className="resultContainer">
+    <div className="result">
+    <div className="close" onClick={()=>setShow(false)}>X</div>
+    <table>
+        <tbody>
+        <tr><th>Type</th><th>Size</th><th>Count</th><th>Amount</th></tr>
+        {orders.map((order)=>(<tr><td>{order.type}</td><td>{order.size}</td><td>{order.count}</td><td><Calculation type={order.type} size={order.size} count={order.count} setTotal={setTotal} UserContext={UserContext}/></td></tr>))}
+        <tr><th colSpan="3">Total</th><th>{total/2}</th></tr>
+        </tbody>
+    </table>
+    </div>
+    </div>:""
+    }
+    </>
+</>)
+}
+
+export default Result;
